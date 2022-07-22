2f9136c9e61b233b0d55df31a9613577f69c07d4
ellen-ls <ellen.l.s1996@gmail.com>
2022-07-22 20:00:00 -0300

    carrinho mais ou menos feito

--- 4food/src/Pages/Cart/cart.js
+++ 4food/src/Pages/Cart/cart.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React,{useState} from "react";
+import { useNavigate } from "react-router-dom";
+import Restaurante from "../Restaurantes/restaurant";
+
+
 
 function Cart(){
+    const navigate = useNavigate();
+    const [comprar,setComprar] = useState([]);
+    
 
+ 
 return(
+    <div>
+        {comprar.map((produto)=>{
+            return <Restaurante key={produto.id} produto={produto}/>
+        })}
     <h2>carrinho</h2>
+    <button onClick={() => navigate("/restaurantes/1")}>Continuar comprando</button>
+
+    
+    </div>
 )
 }
 
